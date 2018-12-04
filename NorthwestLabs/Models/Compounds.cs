5e30213315c6ac4d8ef0e74856c31dab33fb682e
Rob Calcote <robcalcote@gmail.com>
2018-12-04 11:30:11 -0700

    created Model folder and created compound model

---
+++ NorthwestLabs/Models/Compounds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    // Compound Table
+    [Table("Compounds")]
+    public class Compounds
+    {
+        [Key]
+        [Required]
+        [DisplayName("LT Number")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int LTNumber { get; set; }
+
+        [Required]
+        [DisplayName("Compound Name")]
+        public String CompoundName { get; set; }
+
+        [Required]
+        [DisplayName("Maximum Dose")]
+        public float MaximumDose { get; set; }
+    }
+}
