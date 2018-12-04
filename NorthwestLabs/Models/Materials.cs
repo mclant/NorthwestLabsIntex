039258eda30c693502d0673af17875299bc9b17c
Rob Calcote <robcalcote@gmail.com>
2018-12-04 14:32:25 -0700

    added new models and updated existing

---
+++ NorthwestLabs/Models/Materials.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    // Materials Table
+    [Table("Materials")]
+    public class Materials
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Material ID")]
+        [Required(ErrorMessage = "Material ID is required.")]
+        public int MaterialID { get; set; }
+        // Link back to TestMaterials Table
+        public virtual TestMaterials testmaterials { get; set; }
+
+        [DisplayName("Material Name")]
+        [Required(ErrorMessage = "Material Name is required")]
+        [StringLength(30, ErrorMessage = "Material Name must not exceed 30 characters.")]
+        public string MaterialName { get; set; }
+
+        [DisplayName("Material Cost")]
+        [Required(ErrorMessage ="Material Cost is required")]
+        public decimal MaterialCost { get; set; }
+    }
+}
---
+++ NorthwestLabs/Models/TestMaterials.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    // AssayInstances Table
+    [Table("TestMaterials")]
+    public class TestMaterials
+    {
+        #region CompositePrimaryKey
+        [Key, Column(Order = 1)]
+        [DisplayName("Test ID")]
+        [Required(ErrorMessage = "Test ID is required.")]
+        public int TestID { get; set; }
+        // Link back to SampleTests Table
+        public virtual SampleTests sampletests { get; set; }
+
+        [Key, Column(Order = 2)]
+        [DisplayName("Material ID")]
+        [Required(ErrorMessage = "Material ID is required.")]
+        public int MaterialID { get; set; }
+        // Link back to Materials Table
+        public virtual Materials materials { get; set; }
+        #endregion
+
+        [DisplayName("Material Quantity")]
+        [Required(ErrorMessage = "Material Quantity is required")]
+        public int MaterialQty { get; set; }
+    }
+}
