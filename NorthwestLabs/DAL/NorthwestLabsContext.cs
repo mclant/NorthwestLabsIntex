49899bf15be713cd8fd308bbab7eb8efe79eb696
Rob Calcote <robcalcote@gmail.com>
2018-12-04 17:01:07 -0700

    fixed .csproj file to compile the correct models

--- NorthwestLabs/DAL/NorthwestLabsContext.cs
+++ NorthwestLabs/DAL/NorthwestLabsContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Web;
 
-namespace NorthwestLabs.DAL
-{
-    public class NorthwestLabsContext : DbContext
-    {
-        public 
-    }
-}
+////namespace NorthwestLabs.DAL
+////{
+////    public class NorthwestLabsContext : DbContext
+////    {
+////        public 
+////    }
+////}
