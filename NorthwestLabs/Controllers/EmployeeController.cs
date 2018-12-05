b03f537c4ca2bd77c7fd4538d7d8ab2e81b72279
Jon Facer <jface96@gmail.com>
2018-12-05 16:26:48 -0700

    some stuff

--- NorthwestLabs/Controllers/EmployeeController.cs
+++ NorthwestLabs/Controllers/EmployeeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Data.Entity;
 using NorthwestLabs.DAL;
 using NorthwestLabs.Models;
+using System.Net;
 
 namespace NorthwestLabs.Controllers
 {
@@ -46,9 +47,46 @@ namespace NorthwestLabs.Controllers
             return View();
         }
 
-        public ActionResult LabWorker()
+ /// <summary>
+/// LABTECH
+/// </summary>
+        public ActionResult LabTech()
         {
+
             return View();
         }
+
+        public ActionResult QuoteInbox()
+        {
+
+            return View(db.Order.ToList());
+        }
+ 
+        [HttpPost]
+        public ActionResult QuoteInbox(int ordID, [Bind(Include = "OrderID,QuotePrice")] IQueryable<Orders> orders)
+        {
+
+            // List<Orders> ord = new List<Orders>();
+            //orders = db.Order.Where(i => i.OrderID == ordID);
+            //Orders neworder = new Orders();
+            //neworder = ord[0];
+            //.QuotePrice = QuotePrice;
+            //db.Entry(neworder).State = EntityState.Modified;
+            //db.SaveChanges();
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(orders).State = EntityState.Modified;
+                db.SaveChanges();
+                ViewBag.output = "Quote Recorded";
+                return View();
+            }
+
+            return View(db.Order.ToList());
+        }
+        /// <summary>
+        /// LABTECH
+        /// </summary>
+
     }
 }
