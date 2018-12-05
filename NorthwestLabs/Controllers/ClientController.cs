52d06406fdb305629396b7f8cdfea281d02f706e
Matt Lant <matthewclant@gmail.com>
2018-12-05 11:30:27 -0700

    client controller commit

---
+++ NorthwestLabs/Controllers/ClientController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using NorthwestLabs.Models;
+using NorthwestLabs.DAL;
+
+namespace NorthwestLabs.Controllers
+{
+    public class ClientController : Controller
+    {
+        NorthwestLabsContext db = new NorthwestLabsContext();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAQuote()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetAQuote(Customers newcustomer)
+        {
+            newcustomer.logins.LoginUserName = newcustomer.LoginUserName;
+            db.Customer.Add(newcustomer);
+            db.SaveChanges();
+            
+
+            return View();
+        }
+    }
+}
